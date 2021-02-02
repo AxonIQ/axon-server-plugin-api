22cff222c1eb8801ce4c76ce46420b4aa67c9dc8
Marc Gathier <marc.gathier@axoniq.io>
2021-02-02 11:26:09 +0100

    Changed type of defaultvalue to object.

--- src/main/java/io/axoniq/axonserver/extensions/ExtensionPropertyDefinition.java
+++ src/main/java/io/axoniq/axonserver/extensions/ExtensionPropertyDefinition.java
@@ -13,18 +13,18 @@ public class ExtensionPropertyDefinition {
     private final String id;
     private final String name;
     private final int cardinality;
-    private final String defaultValue;
+    private final Object defaultValue;
     private final AttributeType type;
     private final String description;
     private final List<String> optionLabels;
     private final List<String> optionValues;
 
-    public ExtensionPropertyDefinition(String id, String name, int cardinality, String defaultValue,
+    public ExtensionPropertyDefinition(String id, String name, int cardinality, Object defaultValue,
                                        AttributeType type, String description) {
         this(id, name, cardinality, defaultValue, type, description, null, null);
     }
 
-    public ExtensionPropertyDefinition(String id, String name, int cardinality, String defaultValue,
+    public ExtensionPropertyDefinition(String id, String name, int cardinality, Object defaultValue,
                                        AttributeType type, String description, List<String> optionLabels,
                                        List<String> optionValues) {
         this.id = id;
@@ -49,7 +49,7 @@ public class ExtensionPropertyDefinition {
         return cardinality;
     }
 
-    public String defaultValue() {
+    public Object defaultValue() {
         return defaultValue;
     }
 
