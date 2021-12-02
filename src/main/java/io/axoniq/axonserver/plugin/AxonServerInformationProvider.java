b0d18f6b23f2b5530f36ed9942b99ce36b40a4e0
Marc Gathier <marc.gathier@axoniq.io>
2021-12-02 13:24:58 +0100

    service to retrieve Axon Server information from plugin

--- src/main/java/io/axoniq/axonserver/plugin/AxonServerInformationProvider.java
+++ src/main/java/io/axoniq/axonserver/plugin/AxonServerInformationProvider.java
@@ -10,6 +10,8 @@ public interface AxonServerInformationProvider {
 
     String PRODUCT = "product";
     String VERSION = "version";
+    String SIGNATURE = "signature";
+    String SIGNATURE_TIME = "signature_timestamp";
 
     Map<String,String> properties();
 
